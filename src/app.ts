b4b61f115b0d568fd0ec127cd8d1e0c92d31f1b1
Md Siyam <mdsiyam9424@gmail.com>
2026-05-07 13:53:30 +0600

    make noteschema

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,37 @@
 import express, { Application, Request, Response } from 'express';
- 
- 
- 
- const app: Application = express();
+import { model } from 'mongoose';
+import { Schema } from 'mongoose';
 
 
- app.get('/', (req: Request, res: Response) => {
-     res.json("Welcome to Explore Mongoose");
- });
+const app: Application = express();
 
- 
 
- export default app;
+const noteSchema = new Schema({
+    title: String,
+    content: String
+})
+
+const Note = model('Note', noteSchema);
+
+
+app.post('/create-notes', async (req: Request, res: Response) => {
+    const newNote = new Note({
+        title: "First Note",
+        content: "This is the content of the first note"
+    })
+    await newNote.save();
+    res.status(201).json({
+        status: true,
+        message: "Note created successfully",
+        data: newNote
+    });
+
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.json("Welcome to Explore Mongoose");
+});
+
+
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
-import {Server} from 'http';
-import express, {Application} from 'express';
+import { Server } from 'http';
+import express, { Application } from 'express';
 import app from './app';
+import mongoose, { mongo } from 'mongoose';
 let server: Server;
 const port = 5000;
+import dns from 'dns';
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
-
+// todoApp
+// FYfda7i8z4iyp5Of
 
 async function main() {
-    try{
-        
-             server = app.listen(port, () => {
+    try {
+        await mongoose.connect('mongodb+srv://todoApp:FYfda7i8z4iyp5Of@cluster0.ttcu5.mongodb.net/todoApp?appName=Cluster0')
+        console.log("mongodb connected")
+        server = app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
-    }catch(error){
+    } catch (error) {
         console.error(error);
     }
 }
