53aa9f9b4a93ce0bab16210725bfa9904f6f3980
Md Siyam <mdsiyam9424@gmail.com>
2026-05-17 11:08:13 +0600

    make update and delete api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response } from 'express';
-import { model } from 'mongoose';
+import mongoose, { model } from 'mongoose';
 import { Schema } from 'mongoose';
 
 
 const app: Application = express();
 
 
-const noteSchema = new Schema({
+app.use(express.json());
+
+const noteSchema = new mongoose.Schema({
     title: {
         type: String
     },
@@ -16,13 +18,13 @@ const noteSchema = new Schema({
     category: {
         type: String,
         enum: ['Personal', 'Work', 'Other', 'mongodb'],
-        default: 'Personal'
+       default: 'Personal'
     },
 
 },
     { timestamps: true })
 
-const Note = model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema);
 
 
 app.get('/notes', async (req: Request, res: Response) => {
@@ -34,9 +36,52 @@ app.get('/notes', async (req: Request, res: Response) => {
     });
 
 });
+
+// get single note
+app.get('/note/:id', async (req: Request, res: Response) => {
+
+    const id = req.params.id;
+    const query = { _id: id };
+    const note = await Note.findOne(query);
+
+    res.status(200).json({
+        note
+    });
+})
+
+// delete single note
+app.delete('/note/:id', async (req: Request, res: Response) => {
+
+    const id = req.params.id;
+    const query = { _id: id };
+    const note = await Note.findByIdAndDelete(query);
+
+    res.status(200).json({
+        success: true,
+        message: "Note deleted successfully",
+        note
+    });
+})
+
+app.patch('/note/update-note/:id', async (req: Request, res: Response) => {
+
+    const id = req.params.id;
+    const query = { _id: id };
+    const updateData = req.body;
+
+    const note = await Note.findOneAndUpdate(query, updateData, { new: true });
+
+    res.status(200).json({
+        success: true,
+        message: "Note updated successfully",
+        note
+    });
+});
+
 app.post('/note/create-notes', async (req: Request, res: Response) => {
 
     const newNote = req.body; // Assuming the request body contains the note data
+    console.log(newNote)
     // method 1
     // const newNote = new Note({
     //     title: "First Note",
@@ -51,7 +96,7 @@ app.post('/note/create-notes', async (req: Request, res: Response) => {
     res.status(201).json({
         status: true,
         message: "Note created successfully",
-        data: note
+         note
     });
 
 });
